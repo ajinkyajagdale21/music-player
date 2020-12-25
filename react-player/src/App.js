26d1775e8803db70e727cb9cc6facbcc9e6fd8c6
ajinkyajagdale21 <68507834+ajinkyajagdale21@users.noreply.github.com>
2020-12-25 21:22:21 +0530

    update: play& pause

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './styles/app.scss';
 import Player from './components/player';
 import Song from './components/song';
 import data from "./util";
 
 function App() {
+   const [Songs,setSongs]=useState(data());
+   const [currentSong,setCurrentSong]= useState(Songs[2]);
+   const [isPlaying,setIsPlaying]=useState(false);
   return (
     <div className="App">
 
-    <Song />      
-    <Player />
+    <Song currentSong={currentSong}/>      
+    <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong}/>
     
   
     </div>
--- react-player/src/components/player.js
+++ react-player/src/components/player.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React ,{useRef} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPlay,faAngleLeft,faAngleRight} from "@fortawesome/free-solid-svg-icons";
-const Player = ()=>{
+
+const Player = ({currentSong,isPlaying,setIsPlaying})=>{
+  var audioRef= useRef(null);
+  const playSongHandler=()=>{
+   if(isPlaying){
+    audioRef.current.pause(); 
+    setIsPlaying(!isPlaying);
+  }
+   else{
+    audioRef.current.play();
+    setIsPlaying(!isPlaying);
+   }
+  }
+ 
+ 
   return(
     <div className="player">
      
@@ -13,10 +27,10 @@ const Player = ()=>{
   
     <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
         <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
     </div>
-  
+   <audio ref={audioRef} src={currentSong.audio}></audio>
   </div>
   )};
 
